d2ee479bff6aa452669d7dae0d02d37765afefe8
JMcB <joel.mcbride1@live.com>
2021-05-25 18:38:06 +0100

    Add set_pixel

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import requests
 
 
 CONFIG_FILE_PATH = Path('config.json')
+SET_URL = 'https://pixels.pythondiscord.com/set_pixel'
 
 
 def ratelimit(headers):
@@ -19,11 +20,29 @@ def ratelimit(headers):
         time.sleep(requests_reset)
 
 
+def set_pixel(x: int, y: int, rgb: str, headers:dict):
+    payload = {
+        'x': x,
+        'y': y,
+        'rgb': rgb,
+    }
+    r = requests.post(
+        SET_URL,
+        json=payload,
+        headers=headers
+    )
+
+    ratelimit(r.headers)
+
+
 def main():
     with open(CONFIG_FILE_PATH) as config_file:
         config = json.load(config_file)
 
     bearer_token = f"Bearer {config['token']}"
+    headers = {
+        "Authorization": bearer_token
+    }
 
 
 if __name__ == '__main__':
