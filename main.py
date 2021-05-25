6667c230343f52439bebd3410141d30efa4c2fd4
JMcB <joel.mcbride1@live.com>
2021-05-25 17:59:26 +0100

    Add config loading and rate limiting

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python
 
 import json
+import time
+from pathlib import Path
+
 import requests
 
 
+CONFIG_FILE_PATH = Path('config.json')
+
+
+def ratelimit(headers):
+    requests_remaining = int(headers['requests-remaining'])
+    print(f'{requests_remaining} requests remaining')
+    if not requests_remaining:
+        requests_reset = int(headers['requests-reset'])
+        print(f'sleeping for {requests_reset} seconds')
+        time.sleep(requests_reset)
+
+
 def main():
-    pass
+    with open(CONFIG_FILE_PATH) as config_file:
+        config = json.load(config_file)
+
+    bearer_token = f"Bearer {config['token']}"
 
 
 if __name__ == '__main__':
