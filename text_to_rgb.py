aa8dacfcb54109b55717736fed5c6625717ce45d
JMcB <joel.mcbride1@live.com>
2021-05-26 16:00:20 +0100

    Save converted messaeg as image

--- text_to_rgb.py
+++ text_to_rgb.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 
-
+import typing
+import PIL.Image
 from main import three_bytes_to_rgb_hex_string
+
+
+def hex_string_to_tuple(hex_string: str) -> typing.Tuple[int]:
+    converted = []
+    for num in [hex_string[0:2], hex_string[2:4], hex_string[4:6]]:
+        converted.append(int(f'0x{num}', 16))
+    return tuple(converted)
 
 
 def main():
@@ -16,6 +24,15 @@ def main():
         colour = three_bytes_to_rgb_hex_string(text_encoded_padded[i*3:i*3 + 3])
         print(colour)
         colours.append(colour)
+
+    colours_tuples = [hex_string_to_tuple(h) for h in colours]
+    img_bytes = b''
+    for colour_tuple in colours_tuples:
+        img_bytes += bytes(colour_tuple)
+
+    img_size = (len(colours_tuples), 1)
+    img = PIL.Image.frombytes(mode='RGB', size=img_size, data=img_bytes)
+    img.save('test.png')
 
 
 if __name__ == '__main__':
