036fa3019d08d70c531113484a1802bbd5c2f753
JMcB <joel.mcbride1@live.com>
2021-06-05 20:53:03 +0100

    Time for neil

--- main.py
+++ main.py
@@ -22,8 +22,7 @@ __version__ = '3.1.0'
 
 # modify this to change the order of priority or add/remove images
 imgs = [
-    'lost-the-game-no-bg',
-    'lost-the-game',
+    'neil2',
     'cmpc',
     'httpscmpclivetwitchtvcontrolmypc-utf-8',
     'wbub',
@@ -35,6 +34,8 @@ imgs = [
     'pythons2',
     'pride-whole-canvas-mask',
     'pydispix',
+    'lost-the-game-no-bg',
+    'lost-the-game',
 ]
 
 
