dd9a4886cf7c6c15d252d3ce3643e3e2896f8ed2
JMcB <joel.mcbride1@live.com>
2021-06-07 19:44:55 +0100

    wBUB TIME

--- main.py
+++ main.py
@@ -22,8 +22,7 @@ __version__ = '3.1.0'
 
 # modify this to change the order of priority or add/remove images
 imgs = [
-    'cmpc',
-    'httpscmpclivetwitchtvcontrolmypc-utf-8',
+    'wbub2',
 ]
 
 
