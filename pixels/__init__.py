fa23c8a12dbab62b06effce6bbdce91285b7f635
jmcb <joelsgp@protonmail.com>
2022-01-06 16:13:22 +0000

    More refactoring progress on __init__.py

--- pixels/__init__.py
+++ pixels/__init__.py
@@ -2,7 +2,7 @@ import json
 import sys
 import argparse
 import asyncio
-import logging as log
+import logging
 from pathlib import Path
 from typing import Union
 
@@ -13,7 +13,6 @@ from . import discord_mirror
 from . import zone
 
 
-# todo: further cleaning up
 # todo: modularise to support different apis
 # todo: cmpc pixels support
 # todo: better rate limit handling?
@@ -29,21 +28,19 @@ CANVAS_LOG_PATH = Path('canvas.log')
 DEBUG_LOG_PATH = Path('debug.log')
 CANVAS_IMAGE_PATH = IMAGES_FOLDER / 'ignore' / 'canvas.png'
 
-BLANK_PIXEL = 'ffffff'
-
 
 # file handler for all debug logging with timestamps
-file_handler = log.FileHandler(DEBUG_LOG_PATH, encoding='utf-8')
-file_handler.setLevel(log.DEBUG)
-file_formatter = log.Formatter('%(asctime)s:' + log.BASIC_FORMAT)
+file_handler = logging.FileHandler(DEBUG_LOG_PATH, encoding='utf-8')
+file_handler.setLevel(logging.DEBUG)
+file_formatter = logging.Formatter('%(asctime)s:' + logging.BASIC_FORMAT)
 file_handler.setFormatter(file_formatter)
 # stream handler for info level print-like logging
-stream_handler = log.StreamHandler(stream=sys.stdout)
-stream_handler.setLevel(log.INFO)
-stream_formatter = log.Formatter()
+stream_handler = logging.StreamHandler(stream=sys.stdout)
+stream_handler.setLevel(logging.INFO)
+stream_formatter = logging.Formatter()
 stream_handler.setFormatter(stream_formatter)
-log.basicConfig(
-    level=log.DEBUG,
+logging.basicConfig(
+    level=logging.DEBUG,
     handlers=[
         file_handler,
         stream_handler,
@@ -51,7 +48,8 @@ log.basicConfig(
 )
 # don't fill up debug.log with other loggers
 for logger_name in ('urllib3', 'PIL', 'discord'):
-    log.getLogger(logger_name).setLevel(log.ERROR)
+    logging.getLogger(logger_name).setLevel(logging.ERROR)
+log = logging.getLogger(__name__)
 
 
 def get_parser() -> argparse.ArgumentParser:
@@ -71,24 +69,30 @@ def rgb_to_hex(rgb_ints: Union[list[int], bytes]) -> str:
     return '{:0<2x}{:0<2x}{:0<2x}'.format(*rgb_ints)
 
 
+def bytes_to_image(image_bytes: bytes, width: int, height: int) -> Image.Image:
+    return Image.frombytes(
+        mode='RGB',
+        size=(width, height),
+        data=image_bytes
+    )
+
+
 async def save_canvas_as_png(canvas_size, headers, path: Union[str, Path] = None):
     if path is None:
         path = CANVAS_IMAGE_PATH
+    else:
+        path = Path(path)
     path.parent.mkdir(parents=True, exist_ok=True)
 
-    canvas_bytes = await api.get_pixels(canvas_size, headers, as_bytes=True)
-    canvas_image = Image.frombytes(
-        mode='RGB',
-        size=(canvas_size['width'], canvas_size['height']),
-        data=canvas_bytes
-    )
+    canvas_bytes = await api.get_pixels(headers)
+    canvas_image = bytes_to_image(canvas_bytes, canvas_size['width'], canvas_size['height'])
     canvas_image.save(path)
 
 
 async def run_for_zone(z: zone.Zone, canvas_size: dict, headers: dict, bot):
     """Given an img and the location of its top-left corner on the canvas, draw/repair that image."""
     log.info('Getting current canvas status')
-    canvas_bytes = await api.get_pixels(canvas_size, headers, as_bytes=True)
+    canvas_bytes = await api.get_pixels(headers)
     canvas = img_bytes_to_dimensional_list(canvas_bytes, canvas_size)
     log.info('Got current canvas status')
     if bot is not None:
@@ -141,13 +145,13 @@ async def run_protections(zones_to_do: list[zone.Zone], canvas_size: dict, heade
                 log.info(f"img name: {z.name}")
                 log.info(f'img dimension x: {z.width}')
                 log.info(f'img dimension y: {z.height}')
-                log.info(f'img pixels: {z.area_not_transparent}')
-                await run_for_img(z.image, z.location, canvas_size, headers, bot)
+                log.info(f'img pixels: {z.area_opaque}')
+                await run_for_zone(z, canvas_size, headers, bot)
         except Exception as error:
             log.exception(error)
 
 
-async def main():
+async def main_async():
     """Run the program for all imgs."""
     parser = get_parser()
     parser.parse_args()
@@ -176,8 +180,8 @@ async def main():
         bot = None
 
     log.info(f'Loading zones to do from {IMAGES_FOLDER}')
-    zones_to_do = zone.load_zones(IMAGES_FOLDER, imgs)
-    total_area = sum(z.area_not_transparent for z in zones_to_do)
+    zones_to_do = zone.load_zones(IMAGES_FOLDER)
+    total_area = sum(z.area_opaque for z in zones_to_do)
     log.info(f'Total area: {total_area}')
     canvas_area = canvas_size['width'] * canvas_size['height']
     total_area_percent = round(((total_area / canvas_area) * 100), 2)
@@ -188,6 +192,10 @@ async def main():
     await run_protections(zones_to_do, canvas_size, headers, bot)
 
 
+def main():
+    asyncio.run(main_async())
+
+
 if __name__ == '__main__':
     # todo: clean keyboardinterrupt shutdown
-    asyncio.run(main())
+    main()
