2d948d862f9692ed1177606d171a49e5b53886d4
jmcb <joelsgp@protonmail.com>
2022-01-06 18:16:37 +0000

    Finish rewriting discord_mirror.py

--- pixels/discord_mirror.py
+++ pixels/discord_mirror.py
@@ -1,4 +1,5 @@
 import io
+import json
 from datetime import datetime, timezone
 
 import aiohttp
@@ -19,6 +20,7 @@ IMAGE_SCALE = 5
 
 
 # https://discord.com/developers/docs/resources/webhook
+# https://discord.com/developers/docs/reference#uploading-files
 # https://github.com/python-discord/pixels/blob/main/pixels/endpoints/moderation.py
 
 
@@ -31,50 +33,56 @@ def get_embed(now: datetime) -> dict:
     return embed
 
 
-async def create_mirror(webhook_url: str) -> int:
+async def create_mirror(webhook_url: str) -> aiohttp.ClientResponse:
     now = datetime.now(timezone.utc)
     embed = get_embed(now)
-
     payload_json = {
-        'content': '',
         'embeds': [embed],
         'username': WEBHOOK_USERNAME,
         'avatar_url': WEBHOOK_AVATAR_URL,
     }
+    async with aiohttp.ClientSession() as session:
+        return await session.post(url=webhook_url, json=payload_json)
+
+
+async def edit_webhook(stream: io.BytesIO, message_id: int, webhook_url: str) -> aiohttp.ClientResponse:
+    now = datetime.now(timezone.utc)
+    embed = get_embed(now)
+    file_name = FILE_NAME_FORMAT.format(timestamp=now.timestamp())
+    embed['image'] = {'url': f'attachment://{file_name}'}
+
+    attachment = {
+        'id': 0,
+        'description': EMBED_TITLE,
+        'filename': file_name,
+    }
+    payload_json = {
+        'embeds': [embed],
+        'attachments': [attachment],
+        'message_reference': {'message_id': message_id},
+    }
+
+    form_data = aiohttp.FormData()
+    form_data.add_field(
+        name='payload_json',
+        value=json.dumps(payload_json),
+        content_type='application/json'
+    )
+    form_data.add_field(
+        name='file[0]',
+        value=stream.getvalue(),
+        content_type='image/png',
+        filename=file_name
+    )
 
     async with aiohttp.ClientSession() as session:
-        r = await session.post(url=webhook_url, json=payload_json)
-        r_json = await r.json()
-
-    return r_json['id']
+        return await session.patch(url=webhook_url, data=form_data)
 
 
-# async def edit_embed_file(webhook_url: str, embed: dict, stream: io.BytesIO, now: datetime):
-#     file_name = FILE_NAME_FORMAT.format(timestamp=now.timestamp())
-#     embed['image'] = {'url': f'attachment://{file_name}'}
-#     discord_file = {
-#         'name': 'file',
-#         'value': stream.getvalue(),
-#         'filename': file_name,
-#         'content_type': 'application/octet-stream'
-#     }
-#
-#     payload_json = {
-#         'content': '',
-#         'embeds': [embed],
-#         # get rid of the previous attachments
-#         'attachments': []
-#     }
-#
-#     async with aiohttp.ClientSession() as session:
-#         await session.patch(url=webhook_url, data=data)
-#
-#
-# async def update_mirror(canvas: Image.Image, message_id: int, webhook_url: str):
-#     canvas_scaled = util.scale_image(canvas, IMAGE_SCALE)
-#     with io.BytesIO() as byte_stream:
-#         canvas_scaled.save(byte_stream, format='PNG')
-#         byte_stream.seek(0)
-#
-#     now = datetime.now(timezone.utc)
-#     embed = get_embed(now)
+async def update_mirror(canvas: Image.Image, message_id: int, webhook_url: str):
+    canvas_scaled = util.scale_image(canvas, IMAGE_SCALE)
+    with io.BytesIO() as stream:
+        canvas_scaled.save(stream, format='PNG')
+        stream.seek(0)
+
+        return await edit_webhook(stream, message_id, webhook_url)
