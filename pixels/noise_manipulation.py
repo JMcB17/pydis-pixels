4543fc05cfaf499dbe7e33c9a3f5c93e489aac27
jmcb <joelsgp@protonmail.com>
2022-01-06 18:53:05 +0000

    Refactor noise_manipulation.py

--- pixels/noise_manipulation.py
+++ pixels/noise_manipulation.py
@@ -1,24 +1,23 @@
-import random
-import typing
+from random import choices, randint
 
-import main
+from PIL import Image
+import api
 
 
 __version__ = '0.1.0'
-
 
 HEX_CHARS = '0123456789abcdef'
 
 
 def add_noise(canvas_size: dict, headers: dict):
     """Do not use."""
-    x_coord = random.randint(0, canvas_size['width'])
-    y_coord = random.randint(0, canvas_size['height'])
-    colour = ''.join(random.choices(HEX_CHARS, k=6))
-    main.set_pixel(x_coord, y_coord, colour, headers)
+    x_coord = randint(0, canvas_size['width'])
+    y_coord = randint(0, canvas_size['height'])
+    colour = ''.join(choices(HEX_CHARS, k=6))
+    api.set_pixel(x_coord, y_coord, colour, headers)
 
 
-def get_neighbour_pixels(x: int, y: int, img: main.img_type) -> typing.List[str]:
+def get_neighbour_pixels(x: int, y: int, image: Image.Image) -> list[str]:
     """Return the eight pixels that neighbour the given co-ordinates.
 
     Raises IndexError if the pixel is a corner pixel and thus has less than eight neighbours.
@@ -34,23 +33,23 @@ def get_neighbour_pixels(x: int, y: int, img: main.img_type) -> typing.List[str]
                 raise IndexError('pixel is edge pixel')
 
             try:
-                neighbours.append(img[y_neighbour_coord][x_neighbour_coord])
+                neighbours.append(image.getpixel((x_neighbour_coord, y_neighbour_coord)))
             except IndexError as error:
                 raise IndexError('pixel is edge pixel') from error
 
     return neighbours
 
 
-def remove_noise(img: main.img_type, headers: dict, same_neighbour_threshold: int = 7):
+def remove_noise(image: Image.Image, headers: dict, same_neighbour_threshold: int = 7):
     """Try to remove some noise.
 
     If any pixel in img is surrounded by at least same_neighbour_threshold (default 7) of the same colour,
     and pixel is not that colour, set pixel to that colour.
     """
-    for y_coord, row in enumerate(img):
-        for x_coord, pixel in enumerate(row):
+    for y in range(image.height):
+        for x in range(image.width):
             try:
-                pixel_neighbours = get_neighbour_pixels(x_coord, y_coord, img)
+                pixel_neighbours = get_neighbour_pixels(x, y, image)
             except IndexError:
                 continue
 
@@ -58,5 +57,5 @@ def remove_noise(img: main.img_type, headers: dict, same_neighbour_threshold: in
             unique_pixel_neighbours.sort(key=lambda p: pixel_neighbours.count(p), reverse=True)
             highest_incidence_neighbour = unique_pixel_neighbours[0]
             if pixel_neighbours.count(highest_incidence_neighbour) >= same_neighbour_threshold:
-                img[y_coord][x_coord] = highest_incidence_neighbour
-                main.set_pixel(x_coord, y_coord, highest_incidence_neighbour, headers)
+                image.putpixel((x, y), highest_incidence_neighbour)
+                api.set_pixel(x, y, highest_incidence_neighbour, headers)
